11d50809ded30d6ce944b8ef7f120d54279a92b9
AlexandrIv <ias_ric@mail.ru>
2022-11-28 00:19:30 +0300

    add Task10, Task13, Task15, DopTasktoLesson2

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < -999 || (n > -100 && n < 100) || n > 999)
+{
+Console.Write("Вы ошиблись!\nВведите трехзначное число: ");
+n = Convert.ToInt32(Console.ReadLine());
+}
+if (n < 0)
+    n = n *(-1);
+Console.WriteLine ((n % 100) / 10);
